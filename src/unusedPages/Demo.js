8a986431c0432e324dd46866f1be54b94124331c
SvenjaLutze <svenja.lutze@yahoo.de>
2022-06-15 23:50:51 +0200

    added gantt chart changes

--- src/unusedPages/Demo.js
+++ src/unusedPages/Demo.js
@@ -2,18 +2,13 @@ import React, { useEffect } from "react";
 import { DemoNews, DemoSteps, DrawerData } from "../Demo/Data";
 import { AppBar, Container, Grid, Typography } from "@mui/material";
 import Box from "@mui/material/Box";
-import { Epg, Layout } from "planby";
-
-import { useApp } from "../useApp";
 
 import CDrawer from "../Components/CDrawer";
 import CNews from "../Components/CNews";
 import CStepper from "../Components/CStepper";
 import Agenda from "../Components/Agenda";
 import { Studiumplan } from "../Components/Studiumplan";
-import { Timeline } from "../Components/Timeline";
-import { ChannelItem } from "../Components/ChannelItem";
-import { ProgramItem } from "../Components/ProgrammItem";
+import Timeline from "../Components/Timeline";
 import { TaskList } from "../Components/TaskList";
 
 const authmanager = require("../manager/Authmanager").default.getInstance();
@@ -23,8 +18,6 @@ export default function Demo() {
     console.log(authmanager.user);
     console.log(authmanager.userID);
   }, []);
-
-  const { isLoading, getEpgProps, getLayoutProps } = useApp();
 
   return (
     <div style={{ height: "100%", background: "#E6E6E6" }}>
@@ -70,24 +63,7 @@ export default function Demo() {
           <div>
             <h2>Semester 4</h2>
             <div>
-              <div style={{ height: "80vh", width: "100%" }}>
-                <Epg isLoading={isLoading} {...getEpgProps()}>
-                  <Layout
-                    {...getLayoutProps()}
-                    renderTimeline={(props) => <Timeline {...props} />}
-                    renderProgram={({ program, ...rest }) => (
-                      <ProgramItem
-                        key={program.data.id}
-                        program={program}
-                        {...rest}
-                      />
-                    )}
-                    renderChannel={({ channel }) => (
-                      <ChannelItem key={channel.uuid} channel={channel} />
-                    )}
-                  />
-                </Epg>
-              </div>
+              <Timeline />
             </div>
           </div>
         </div>
