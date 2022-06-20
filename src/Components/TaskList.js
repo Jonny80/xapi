965daa0d6267640a5d2c13e777554e0344fa1525
SymeX16 <dustin.heyer08@gmail.com>
2022-06-20 20:53:10 +0200

    Todos lassen sich speichern links

--- src/Components/TaskList.js
+++ src/Components/TaskList.js
@@ -10,43 +10,153 @@ import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import Button from "@mui/material/Button";
 import {months} from "../Demo/Data";
-
+const authmanager = require("../manager/Authmanager").default.getInstance();
 
 export const TaskList = () => {
 
-    const initialList = [
-    {
-      name: "Thomas",
-      time: "12:54",
-      date: new Date(),
-    },
-  ];
-  const [list, setList] = React.useState(initialList);
+  React.useEffect(() => {
+    start();
+  }, []);
+
+  var Minio = require("minio");
+  var minioClient = new Minio.Client({
+    endPoint: "141.56.132.18",
+    port: 9000,
+    useSSL: false,
+    accessKey: "admin",
+    secretKey: "hgjkrwehg46782h87z",
+  });
+
+  function pushChangedDataToServer(value) {
+    var arr = [];
+    minioClient.getObject("status", "data.json", function(err, dataStream) {
+      if (err) {
+        return console.log("Failed to connect to server!");
+      }
+      dataStream.on("data", function(chunk) {
+        arr.push(chunk);
+      });
+      dataStream.on("end", function() {
+        var reg = JSON.parse(arr.toString()).user;
+        let tochange = null;
+        reg.map((u, i) => {
+          if (`${u.id}` === `${value.id}`) {
+            console.log("changed");
+            tochange = i;
+          }
+        });
+        if (tochange !== null) {
+          reg[tochange] = value;
+        }
+        upload(reg);
+      });
+    });
+  }
+  function upload(u) {
+    var t = '{"user":' + JSON.stringify(u) + "}";
+    minioClient.putObject("status", "data.json", t, t.size, function(
+      err,
+      objInfo
+    ) {
+      if (err) {
+        return console.log(err); // err should be null
+      }
+      console.log("Success", objInfo);
+    });
+  }
+
+var initialList=[];
+
+  function start() {
+    console.log("starten wir");
+  //  console.log(new Date);
+    var arr = [];
+  
+
+    minioClient.getObject("status", "data.json", function(err, dataStream) {
+      if (err) {
+        return console.log(err);
+      }
+      dataStream.on("data", function(chunk) {
+        arr.push(chunk);
+      });
+      dataStream.on("end", function() {
+        //  console.log(arr.toString())
+        var u = JSON.parse(arr.toString()).user;
+        //   console.log("Redy")
+
+        for (var i = 0; i < u.length; i++) {
+          if (`${u[i].id}` === `${authmanager.userID}`) {
+            setUserData(u[i]);
+         //   console.log(u[i])
+            if (u[i].todo !== undefined) {
+          //    console.log(u[i].todo);
+              setTodo(u[i].todo);
+              setList(u[i].todo)
+              
+            }else{
+              initialList = [
+                {
+                  name: "Thomas",
+                  time: "12:54",
+                  date: new Date(),
+                },
+              ];
+            }
+          }
+        }
+      });
+    });
+  }
+
+
+     
+  
+  const [list, setList] = React.useState([]);
   const [name, setName] = React.useState("");
   const [time, setTime] = React.useState("");
   const [value, setDate] = React.useState(new Date());
+  const [todo, setTodo] = React.useState([]);
+  const [userData, setUserData] = React.useState(null);
 
   function handleNameChange(event) {
-    console.log(event.target.value);
+  //  console.log(event.target.value);
     setName(event.target.value);
   }
   function handleTimeChange(event) {
     setTime(event.target.value);
   }
   function handleDateChange(event) {
-    console.log(event);
+  //  console.log(event);
     setDate(event);
   }
 
   function handleAdd() {
     const date = value.toISOString();
-    const newList = list.concat({ name:name, time:time, date:value });
+    
+    //console.log("hier" +value)
+    //console.log("h"+formatDate(new Date()).split(" ")[0])
+    
+    const newList = list.concat({ name:name, time:time, date:formatDate(value).split(" ")[0] });
 
     setList(newList);
 
     setName("");
     setDate(new Date());
     setTime("");
+
+    var ud = null;
+    if (userData.todo === undefined || userData.todo === null) {
+      ud = userData;
+      ud.todo = newList;
+      setUserData(ud);
+    } else {
+      ud = userData;
+      ud.todo=newList;
+      setUserData(ud);
+    }
+    pushChangedDataToServer(ud);
+
   }
 
   function getMonth(date) {
@@ -60,8 +170,8 @@ export const TaskList = () => {
 
 
   function formatDate(date) {
-      console.log(list)
-      console.log(typeof date)
+      //console.log(list)
+     //console.log(typeof date)
       let year = date.getFullYear();
       let month = ("0" + (date.getMonth() + 1)).slice(-2);
       let day = ("0" + date.getDate()).slice(-2);
@@ -139,7 +249,7 @@ export const TaskList = () => {
               </div>
             </AccordionSummary>
             <AccordionDetails>
-              <Typography>{formatDate(item.date)} {item.name}</Typography>
+              <Typography>{item.date} {item.name}</Typography>
             </AccordionDetails>
           </Accordion>
         ))}
