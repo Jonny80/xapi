bf489712e0b43e4fbe95b349d5a687540d3bb332
janhandl <jh@alphainside.eu>
2022-06-30 02:05:26 +0200

    added built version

---
+++ app/precache-manifest.7ecbd54d0767d755d9e391179c67c8b6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b98e1b095d86f1d5f5cc",
+    "url": "/static/css/main.019e7dd1.chunk.css"
+  },
+  {
+    "revision": "b98e1b095d86f1d5f5cc",
+    "url": "/static/js/main.9bf02f6a.chunk.js"
+  },
+  {
+    "revision": "b98615f3c3d6ef1905fb",
+    "url": "/static/js/runtime~main.dd2fc775.js"
+  },
+  {
+    "revision": "bd2ce3dc15e91d8709fc",
+    "url": "/static/css/2.e53210d3.chunk.css"
+  },
+  {
+    "revision": "bd2ce3dc15e91d8709fc",
+    "url": "/static/js/2.55c8100a.chunk.js"
+  },
+  {
+    "revision": "f7af6d92c0ab64a993fe",
+    "url": "/static/js/3.03154e0b.chunk.js"
+  },
+  {
+    "revision": "cff377b0b30749b6de48adf051737898",
+    "url": "/static/media/result.cff377b0.svg"
+  },
+  {
+    "revision": "e37f92f3a878e28a4dd25054f3c8c710",
+    "url": "/index.html"
+  }
+];
