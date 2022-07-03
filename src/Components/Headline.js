d68ffd7fc5ecda04ec9f4117b162cf1bda24d4a4
MarkusCMC <markus-stein@outlook.com>
2022-07-03 15:19:51 +0200

    changed view-switch behaviour
    - button text changes depending on visible screen
    - modules are now hidden in basic übersicht

--- src/Components/Headline.js
+++ src/Components/Headline.js
@@ -35,12 +35,20 @@ function switchModul(v){
         </div>
         <div className='subhead'>
             <p id='name'>043 Medieninformatik</p>
+            {studienplanStatus &&
+            <Button  onClick={changeState} id="studgang" variant="text" className="button" >
+                Modulübersicht
+            </Button>
+            }
+            {!studienplanStatus &&
             <Button  onClick={changeState} id="studgang" variant="text" className="button" >
                 Studiengangsübersicht
             </Button>
+            }
        
         </div>
-        <div id="modulContainer">
+        {!studienplanStatus &&
+            <div id="modulContainer">
             <Button value={1} onClick={(e)=>{switchModul(e.target.value)}} id="navModuls1">IT I</Button>
             <Button value={2} onClick={(e)=>{switchModul(e.target.value)}}id="navModuls2">SE II</Button>
             <Button value={3} onClick={(e)=>{switchModul(e.target.value)}}id="navModuls3">AVS</Button>
@@ -50,6 +58,8 @@ function switchModul(v){
             <Button value={7} onClick={(e)=>{switchModul(e.target.value)}}id="navModuls7">GIS</Button>
             <Button value={8} onClick={(e)=>{switchModul(e.target.value)}}id="navModuls8">INFOVIS</Button>
             </div>
+        }
+        
     </div>
   )
 }
